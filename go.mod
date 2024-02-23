0bccfac7b483e6db81b6aa09074312c33aaa9534
zhouyp <zhouyp@91160.com>
2024-02-23 15:42:16 +0800

    feat：阿里系签名函数

--- go.mod
+++ go.mod
@@ -3,7 +3,12 @@ module github.com/bobo-rs/mall-util
 go 1.20
 
 require (
+	github.com/dlclark/regexp2 v1.10.0 // indirect
+	github.com/dop251/goja v0.0.0-20240220182346-e401ed450204 // indirect
+	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
 	github.com/gogf/gf/v2 v2.6.2 // indirect
+	github.com/google/pprof v0.0.0-20240207164012-fb44976bdcd5 // indirect
 	go.opentelemetry.io/otel v1.22.0 // indirect
 	go.opentelemetry.io/otel/trace v1.22.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
--- utli/util_test.go
+++ utli/util_test.go
@@ -34,3 +34,10 @@ func Test_Calculate(t *testing.T) {
 	fmt.Println(Float64("8888.99"), Float64("hh999999"))
 
 }
+
+func Test_1688Sign(t *testing.T) {
+	// 223f39ef64a35582a1b48ed2f1aecace_1708680687662
+	fmt.Println(`1688原加密值：3d592bc663ac75436b5ab5547731f3a3`)
+	var sign = `223f39ef64a35582a1b48ed2f1aecace&1708672769659&12574478&{"dataType":"moduleData","argString":"{\"appName\":\"offerlistAlisite\",\"memberId\":\"b2b-3930917912e7bdf\",\"appdata\":{\"sortType\":\"ninetytradenumdowndesc\",\"count\":2,\"showMode\":\"single\"}}"}`
+	fmt.Println(Sign1688(sign))
+}
